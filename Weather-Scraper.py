1e0cccef1edd9a2c75a9331a159362e0ce51f71a
Mason Wilhoite <masont.wilhoite@gmail.com>
2022-04-25 00:22:32 -0500

    first commit

---
+++ Weather-Scraper.py
@@ -0,0 +1,18 @@
+#A simple script to scrape weather data utilizing Python.
+
+
+from requests_html import HTMLSession
+
+s = HTMLSession()
+query = 'Lebanon, TN'
+url = f'https://www.google.com/search?client=firefox-b-1-d&q=weather+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0'})
+
+#Data gathered
+temp = r.html.find('span#wob_tm', first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+#Output
+print(query, temp, unit, desc)
