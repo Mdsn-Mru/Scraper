a44fd2ce3192bfcde54be83fefc7fe095d182e76
Mason Wilhoite <masont.wilhoite@gmail.com>
2022-04-25 12:10:03 -0500

    Slight comment cleaning

--- Weather-Scraper.py
+++ Weather-Scraper.py
@@ -4,15 +4,16 @@
 from requests_html import HTMLSession
 
 s = HTMLSession()
-query = 'Lebanon, TN'
+query = 'Baltimore, Maryland'
 url = f'https://www.google.com/search?q=weather+{query}'
 
+#Server request
 r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0'})
 
-#Data gathered
+#Parse the html
 temp = r.html.find('span#wob_tm', first=True).text
 unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
 desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
 
-#Output
+#Print out scraped data
 print(query, temp, unit, desc)
